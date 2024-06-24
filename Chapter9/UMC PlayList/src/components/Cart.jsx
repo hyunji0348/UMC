b5dfd7ff1d76487a9ad1fd83099d7ddeb3d1ebdd
김현지 <@naver.com>
2024-06-24 13:14:23 +0900

    10주차

--- Chapter9/UMC PlayList/src/components/Cart.jsx
+++ Chapter9/UMC PlayList/src/components/Cart.jsx
@@ -1,43 +1,61 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
-import { increase, decrease, removeItem, clearCart, calculateTotals } from '../redux/cartSlice';
 import CartItem from './CartItem';
 import { CartIcon } from '../constants/icons';
+import { fetchCartItems, clearCart } from '../redux/cartSlice';
 
 const Cart = () => {
-  const dispatch = useDispatch();
-  const { cartItems, totalAmount, totalPrice } = useSelector(state => state.cart);
-
-  useEffect(() => {
-    dispatch(calculateTotals());
-  }, [cartItems, dispatch]);
-
-  if (cartItems.length === 0) {
+    const dispatch = useDispatch();
+    const { cartItems, totalAmount, totalPrice, status, error } = useSelector((state) => state.cart);
+  
+    useEffect(() => {
+      dispatch(fetchCartItems());
+    }, [dispatch]);
+  
+    if (status === 'loading') {
+      return <LoadingContainer>로딩 중...</LoadingContainer>;
+    }
+  
+    if (status === 'failed') {
+      return <ErrorContainer>에러: {error}</ErrorContainer>;
+    }
+  
+    if (cartItems.length === 0) {
+      return (
+        <Container>
+          <h2>
+            <CartIconWrapper>
+              <CartIcon />
+            </CartIconWrapper>{' '}
+            장바구니
+          </h2>
+          <EmptyCart>장바구니에 음악이 없습니다.</EmptyCart>
+        </Container>
+      );
+    }
+  
     return (
       <Container>
-        <h2><CartIconWrapper><CartIcon /></CartIconWrapper> 장바구니</h2>
-        <EmptyCart>장바구니에 음악이 없습니다.</EmptyCart>
+        <h2>
+          <CartIconWrapper>
+            <CartIcon />
+          </CartIconWrapper>{' '}
+          장바구니
+        </h2>
+        <Items>
+          {cartItems.map((item) => (
+            <CartItem key={item.id} item={item} />
+          ))}
+        </Items>
+        <CartFooter>
+          <h4>총 수량: {totalAmount}</h4>
+          <h4>총 가격: {totalPrice}원</h4>
+          <ClearCartButton onClick={() => dispatch(clearCart())}>장바구니 비우기</ClearCartButton>
+        </CartFooter>
       </Container>
     );
-  }
-
-  return (
-    <Container>
-      <h2><CartIconWrapper><CartIcon /></CartIconWrapper> 장바구니</h2>
-      <Items>
-        {cartItems.map(item => (
-          <CartItem key={item.id} item={item} />
-        ))}
-      </Items>
-      <CartFooter>
-        <h4>총 수량: {totalAmount}</h4>
-        <h4>총 가격: {totalPrice}원</h4>
-        <ClearCartButton onClick={() => dispatch(clearCart())}>장바구니 비우기</ClearCartButton>
-      </CartFooter>
-    </Container>
-  );
-};
+  };
 
 export default Cart;
 
@@ -79,4 +97,15 @@ const ClearCartButton = styled.button`
 const CartIconWrapper = styled.span`
   width: 1.5rem; /* 아이콘 크기 조절 */
   height: auto;
+`;
+
+const LoadingContainer = styled.div`
+  padding: 2rem;
+  text-align: center;
+`;
+
+const ErrorContainer = styled.div`
+  padding: 2rem;
+  text-align: center;
+  color: red;
 `;
--- Chapter9/UMC PlayList/src/redux/cartSlice.jsx
+++ Chapter9/UMC PlayList/src/redux/cartSlice.jsx
@@ -1,26 +1,44 @@
-import { createSlice } from '@reduxjs/toolkit';
-import cartItems from '../constants/cartItems';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
+// 장바구니 아이템 초기 상태
 const initialState = {
-  cartItems: cartItems,
+  cartItems: [],
   totalAmount: 0,
   totalPrice: 0,
+  status: 'idle',
+  error: null,
 };
 
+// 서버에서 장바구니 아이템을 가져오는 비동기 thunk
+export const fetchCartItems = createAsyncThunk('cart/fetchCartItems', async () => {
+  try {
+    const response = await axios.get('http://localhost:8080/musics');
+    return response.data;
+  } catch (error) {
+    throw Error('장바구니 아이템을 불러오는 중 에러가 발생했습니다.');
+  }
+});
+
+// slice 생성
 const cartSlice = createSlice({
   name: 'cart',
   initialState,
   reducers: {
     increase: (state, action) => {
       const item = state.cartItems.find(item => item.id === action.payload);
-      item.amount += 1;
+      if (item) {
+        item.amount += 1;
+      }
     },
     decrease: (state, action) => {
       const item = state.cartItems.find(item => item.id === action.payload);
-      if (item.amount > 1) {
-        item.amount -= 1;
-      } else {
-        state.cartItems = state.cartItems.filter(item => item.id !== action.payload);
+      if (item) {
+        if (item.amount > 1) {
+          item.amount -= 1;
+        } else {
+          state.cartItems = state.cartItems.filter(item => item.id !== action.payload);
+        }
       }
     },
     removeItem: (state, action) => {
@@ -40,7 +58,22 @@ const cartSlice = createSlice({
       state.totalPrice = total;
     },
   },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchCartItems.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchCartItems.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.cartItems = action.payload;
+      })
+      .addCase(fetchCartItems.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      });
+  },
 });
 
 export const { increase, decrease, removeItem, clearCart, calculateTotals } = cartSlice.actions;
+
 export default cartSlice.reducer;
